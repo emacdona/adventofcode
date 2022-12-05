4a21a7fec512e0c8ceb4c84c1882e67c704d8757
Ed <edmacdonald555@gmail.com>
2022-12-05 12:54:02 +0000

    Better output; output both problems answers at once

--- 05/main.js
+++ 05/main.js
@@ -6,7 +6,8 @@ import { stdin as input } from 'node:process';
 // Answer 1 based on my input: RFFFWBPNS
 // Answer 2 based on my input: CQQBBJFCS
 (function(context){
-   var stacks = [];
+   var stacks1 = [];
+   var stacks2 = [];
 
    readline
       .createInterface({input})
@@ -15,45 +16,39 @@ import { stdin as input } from 'node:process';
          if(input.match(/^\s*\[/)){
             var stack = 0;
             for(var i=1; i<input.length; i+=4){
-               if(stacks[stack] === undefined){
-                  stacks[stack] = [];
+               if(stacks1[stack] === undefined){
+                  stacks1[stack] = [];
+                  stacks2[stack] = [];
                }
                if(input[i] != " "){
-                  stacks[stack].push(input[i]);
+                  stacks1[stack].push(input[i]);
+                  stacks2[stack].push(input[i]);
                }
                stack++;
             }
-
-            /*
-            for(var i=0; i<stacks.length; i++){
-               console.log(`stack ${i}: ${stacks[i]}`);
-            }
-            */
          }
          // move instructions
          else if(input.match(/^move /)){
             const [unused, quantity, source, destination] = 
                input.match(/^move\s+(\d+)\s+from\s+(\d+)\s+to\s+(\d+)/);
-            //console.log(`${quantity}:${source}:${destination}`);
             
             // Uncomment to get answer for #1
-            //for(var i=0; i<quantity; i++){
-            //   stacks[destination-1].unshift(stacks[source-1].shift());
-            //}
+            for(var i=0; i<quantity; i++){
+               stacks1[destination-1].unshift(stacks1[source-1].shift());
+            }
             
             // Uncomment to get answer for #2
             var tmp = []
             for(var i=0; i<quantity; i++){
-               tmp.push(stacks[source-1].shift());
+               tmp.push(stacks2[source-1].shift());
             }
             for(var i=0; i<quantity; i++){
-               stacks[destination-1].unshift(tmp.pop());
+               stacks2[destination-1].unshift(tmp.pop());
             }
          }
       })
       .on('close', () => {
-         for(var i = 0; i < stacks.length; i++){
-            console.log(stacks[i][0]);
-         }
+         console.log("Problem 1:", stacks1.map(x=>x[0]).join(''));
+         console.log("Problem 2:", stacks2.map(x=>x[0]).join(''));
       })
 })({})
